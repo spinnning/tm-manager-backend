9fab25a2ea376c51e92d1ea60314eeebe2b158b7
spinnning <maximilian.hilmbauer@sop.co.at>
2022-06-22 20:40:08 +0200

    Commit on Master 22.06.2022

--- src/main/java/com/example/tmmanagerbackend/student/StudentController.java
+++ src/main/java/com/example/tmmanagerbackend/student/StudentController.java
@@ -1,29 +1,26 @@
 package com.example.tmmanagerbackend.student;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/student")
 public class StudentController {
 
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
     @GetMapping
     public List<Student> getStudents() {
-        return List.of(
-            new Student(
-                1L,
-                "Mariam",
-                "mariam.jamal@gmail.com",
-                21,
-                LocalDate.of(2000, Month.JANUARY, 5)
-            )
-        );
+        return studentService.getStudents();
     }
 
 }
